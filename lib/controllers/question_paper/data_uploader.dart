5e531b32689c44c26c2689aad2dc71794a0f7673
uemirhanselim <uemirhanselim@gmail.com>
2022-10-15 12:56:42 +0300

    dot added

--- lib/controllers/question_paper/data_uploader.dart
+++ lib/controllers/question_paper/data_uploader.dart
@@ -1,7 +1,8 @@
 import 'package:get/get.dart';
+
 //? GetxContoller, ChangeNotifier ile aynı sanırım
 class DataUploader extends GetxController {
-  //? onReady() fonksiyonu yalnıza bir kez çağırılır, hot restart yaparsan birdaha çağırmaz hot reload lazım.
+  //? onReady() fonksiyonu yalnıza bir kez çağırılır, hot restart yaparsan birdaha çağırmaz hot reload lazım..
   @override
   void onReady() {
     uploadData();
