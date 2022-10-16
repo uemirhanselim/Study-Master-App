70b509885b3e9d28fe5e30d15c2c3ecfc5b11c68
uemirhanselim <uemirhanselim@gmail.com>
2022-10-16 10:34:13 +0300

    specific Local json files fetched and printed

--- lib/controllers/question_paper/data_uploader.dart
+++ lib/controllers/question_paper/data_uploader.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 //? GetxContoller, ChangeNotifier ile aynı sanırım
@@ -9,7 +13,17 @@ class DataUploader extends GetxController {
     super.onReady();
   }
 
-  void uploadData() {
-    print("Data is uploading...");
+  Future<void> uploadData() async {
+    final manifestContent = await DefaultAssetBundle.of(Get.context!)
+        .loadString("AssetManifest.json");
+    final Map<String, dynamic> manifestMap = json.decode(manifestContent);
+    final papersInAssets = manifestMap.keys
+        .where((path) =>
+            path.startsWith("assets/DB/papers") && path.contains(".json"))
+        .toList();
+    for (var paper in papersInAssets) {
+      String stringPaperContent = await rootBundle.loadString(paper);
+      print(stringPaperContent);
+    }
   }
 }
