706bbafc4239812400c3a8134821b42188986dd7
uemirhanselim <uemirhanselim@gmail.com>
2022-10-16 11:11:31 +0300

    questionpapermodel class created

---
+++ lib/models/question_paper_model.dart
@@ -0,0 +1,84 @@
+class QuestionPaperModel {
+  String id;
+  String title;
+  String? imageUrl;
+  String description;
+  int timeSeconds;
+  List<Questions>? questions;
+
+  QuestionPaperModel(
+      {required this.id,
+      required this.title,
+      this.imageUrl,
+      required this.description,
+      required this.timeSeconds,
+      this.questions});
+
+  QuestionPaperModel.fromJson(Map<String, dynamic> json)
+      : id = json['id'] as String,
+        title = json['title'] as String,
+        imageUrl = json['image_url'] as String,
+        description = json['Description'] as String,
+        timeSeconds = json['time_seconds'],
+        questions = (json['questions'] as List)
+            .map((dynamic e) => Questions.fromJson(e as Map<String, dynamic>))
+            .toList();
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['title'] = title;
+    data['image_url'] = imageUrl;
+    data['Description'] = description;
+    data['time_seconds'] = timeSeconds;
+
+    return data;
+  }
+}
+
+class Questions {
+  String id;
+  String question;
+  List<Answers> answers;
+  String? correctAnswer;
+
+  Questions(
+      {required this.id,
+      required this.question,
+      required this.answers,
+      this.correctAnswer});
+
+  Questions.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        question = json['question'],
+        answers =
+            (json['answers'] as List).map((e) => Answers.fromJson(e)).toList(),
+        correctAnswer = json['correct_answer'];
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['question'] = question;
+    data['answers'] = answers.map((v) => v.toJson()).toList();
+    data['correct_answer'] = correctAnswer;
+    return data;
+  }
+}
+
+class Answers {
+  String? identifier;
+  String? answer;
+
+  Answers({this.identifier, this.answer});
+
+  Answers.fromJson(Map<String, dynamic> json)
+      : identifier = json['identifier'],
+        answer = json['Answer'];
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['identifier'] = identifier;
+    data['Answer'] = answer;
+    return data;
+  }
+}
