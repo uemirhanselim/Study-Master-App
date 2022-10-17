4faec33bf54a647ae3ead4eb965756cc2afcdef0
uemirhanselim <uemirhanselim@gmail.com>
2022-10-17 23:03:54 +0300

    questions and answers uploaded to backend

--- lib/controllers/question_paper/data_uploader.dart
+++ lib/controllers/question_paper/data_uploader.dart
@@ -42,7 +42,24 @@ class DataUploader extends GetxController {
         "questions_count":
             paper.questions == null ? 0 : paper.questions!.length,
       });
+
+      for (var questions in paper.questions!) {
+        final questionPath =
+            questionRF(paperId: paper.id, questionId: questions.id);
+        batch.set(questionPath, {
+          "question": questions.question,
+          "correct_answer": questions.correctAnswer,
+        });
+
+        for (var answer in questions.answers) {
+          batch.set(questionPath.collection("answers").doc(answer.identifier), {
+            "identifier": answer.identifier,
+            "answer": answer.answer,
+          });
+        }
+      }
     }
+
     await batch.commit();
   }
 }
