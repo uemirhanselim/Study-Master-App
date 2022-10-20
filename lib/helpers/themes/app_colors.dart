469b755af368e8057f0aca8454aab8790726a6fb
uemirhanselim <uemirhanselim@gmail.com>
2022-10-20 23:49:02 +0300

    app gradient colors created with condition

---
+++ lib/helpers/themes/app_colors.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_master_class/helpers/themes/app_light_theme.dart';
+import 'package:firebase_master_class/helpers/themes/theme_manager.dart';
+import 'package:flutter/material.dart';
+
+import 'app_dark_theme.dart';
+
+const mainGradientLight = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [
+    primaryLightColorLight,
+    primaryColorLight,
+  ],
+);
+
+const mainGradientDark = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [
+    primaryDarkColorDark,
+    primaryColorDark,
+  ],
+);
+
+LinearGradient mainGradient(BuildContext context) =>
+    ThemeManager.isDarkMode(context) ? mainGradientDark : mainGradientLight;
