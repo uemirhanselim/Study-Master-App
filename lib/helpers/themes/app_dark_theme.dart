843f750cf7a35fc5f73ebdb695e9650c3eb8b74c
uemirhanselim <uemirhanselim@gmail.com>
2022-10-20 17:32:34 +0300

    light and dark theme linear colors created

---
+++ lib/helpers/themes/app_dark_theme.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/animation.dart';
+
+const Color primaryDarkColorDark = Color(0xff2e3c62);
+const Color primaryColorDark = Color(0xff99ace1); //TODO: change it later
---
+++ lib/helpers/themes/app_light_theme.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/animation.dart';
+
+const Color primaryLightColorLight = Color(0xff3ac3cb);
+const Color primaryColorLight = Color(0xfff85187); //TODO: change it later
