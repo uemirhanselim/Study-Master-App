a759d2fef200a1f9441f51b6dd63f30d35d65ca9
uemirhanselim <uemirhanselim@gmail.com>
2022-10-20 17:34:13 +0300

    routes give it to the routes list

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_master_class/view/introduction/introduction_view.dart';
 import 'package:firebase_master_class/view/splash/splash_view.dart';
 import 'package:get/get.dart';
 
 class AppRoutes {
   static List<GetPage> routes() => [
-    GetPage(name: "/", page: () => const SplashView())
-  ];
+        GetPage(name: "/", page: () => const SplashView()),
+        GetPage(name: "/introduction", page: () => const IntroductionView ()),
+      ];
 }
