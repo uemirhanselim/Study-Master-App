25858f69c9878b0edae7a42f5b44b2b683453918
uemirhanselim <uemirhanselim@gmail.com>
2022-10-21 21:48:05 +0300

    themeController injected inside dependencies()

--- lib/bindings/initial_bindings.dart
+++ lib/bindings/initial_bindings.dart
@@ -1,10 +1,12 @@
 import 'package:firebase_master_class/controllers/auth_contoller.dart';
+import 'package:firebase_master_class/controllers/theme_controller.dart';
 import 'package:get/get.dart';
 
 class InitialBindings implements Bindings {
   @override
   void dependencies() {
     //? bir controllerı inject etmek için put() metodunu kullan
+    Get.put(ThemeController());
     Get.put(AuthContoller(), permanent: true);
   }
 }
