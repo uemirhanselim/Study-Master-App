622ffd83fdfad5dcad67534f35e79eac302eb2e9
uemirhanselim <uemirhanselim@gmail.com>
2022-10-21 20:50:20 +0300

    changes amde on app_circle_button

--- lib/widgets/app_circle_button.dart
+++ lib/widgets/app_circle_button.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 class AppCircleButton extends StatelessWidget {
-  const AppCircleButton({super.key, required this.child, this.color, this.width = 60.0, this.onTap});
+  const AppCircleButton(
+      {super.key,
+      required this.child,
+      this.color,
+      this.width = 60.0,
+      this.onTap});
   final Widget child;
   final Color? color;
   final double width;
@@ -10,6 +15,8 @@ class AppCircleButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
+      type: MaterialType.transparency,
+      clipBehavior: Clip.hardEdge,
       shape: const CircleBorder(),
       child: InkWell(
         onTap: onTap,
