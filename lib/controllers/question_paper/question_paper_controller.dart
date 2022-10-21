2a545062171fca6679633cd52b0d96d85194ea60
uemirhanselim <uemirhanselim@gmail.com>
2022-10-21 23:40:08 +0300

    question_paper_controller created

---
+++ lib/controllers/question_paper/question_paper_controller.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_master_class/services/firebase_storage_service.dart';
+import 'package:get/get.dart';
+
+class QuestionPaperController extends GetxController {
+  final allPaperImages = <String>[].obs;
+
+  @override
+  void onReady() {
+    getAllPapers();
+    super.onReady();
+  }
+
+  Future<void> getAllPapers() async {
+    List<String> imgName = [
+      "biology",
+      "chemistry",
+      "math",
+      "physics",
+    ];
+
+    try {
+      for (var img in imgName) {
+        final imgUrl = await Get.find<FirebaseStorageService>().getImage(img);
+        allPaperImages.add(imgUrl!);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+}
