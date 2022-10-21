b09efd460dae02f7f0603b80c057e1b06408f004
uemirhanselim <uemirhanselim@gmail.com>
2022-10-21 23:39:52 +0300

    basic homeView created

---
+++ lib/view/home/home_view.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_master_class/controllers/question_paper/question_paper_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    QuestionPaperController questionPaperController = Get.find();
+    return Scaffold(
+      body: ListView.separated(
+          itemBuilder: (context, index) {
+            return ClipRRect(
+              child: SizedBox(
+                height: 200,
+                width: 200,
+                child: FadeInImage(
+                    placeholder:
+                        const AssetImage("assets/images/app_splash_logo.png"),
+                    image: NetworkImage(
+                        questionPaperController.allPaperImages[index])),
+              ),
+            );
+          },
+          separatorBuilder: (context, index) => const SizedBox(height: 20),
+          itemCount: questionPaperController.allPaperImages.length),
+    );
+  }
+}
