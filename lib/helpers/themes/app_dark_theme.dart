46a4c06d90f0e07a41df2718a32c10e53d612a56
uemirhanselim <uemirhanselim@gmail.com>
2022-10-21 21:39:19 +0300

    dark and light themes are created

--- lib/helpers/themes/app_dark_theme.dart
+++ lib/helpers/themes/app_dark_theme.dart
@@ -1,4 +1,19 @@
-import 'package:flutter/animation.dart';
+import 'package:firebase_master_class/helpers/themes/sub_theme_data_mixin.dart';
+import 'package:flutter/material.dart';
 
 const Color primaryDarkColorDark = Color(0xff2e3c62);
 const Color primaryColorDark = Color(0xff99ace1); //TODO: change it later
+const Color mainTextColorDark = Colors.white;
+
+class DarkTheme with SubThemeData {
+  ThemeData buildDarkTheme() {
+    final ThemeData systemDarkTheme = ThemeData.dark();
+    return systemDarkTheme.copyWith(
+      iconTheme: getIconTheme(),
+      textTheme: getTextTheme().apply(
+        bodyColor: mainTextColorDark,
+        displayColor: mainTextColorDark,
+      ),
+    );
+  }
+}
--- lib/helpers/themes/app_light_theme.dart
+++ lib/helpers/themes/app_light_theme.dart
@@ -1,4 +1,19 @@
-import 'package:flutter/animation.dart';
+import 'package:firebase_master_class/helpers/themes/sub_theme_data_mixin.dart';
+import 'package:flutter/material.dart';
 
 const Color primaryLightColorLight = Color(0xff3ac3cb);
 const Color primaryColorLight = Color(0xfff85187); //TODO: change it later
+const Color mainTextColoLight = Color.fromARGB(255, 40, 40, 40);
+
+class LightTheme with SubThemeData {
+  buildLightTheme() {
+    final ThemeData systemLightTheme = ThemeData.light();
+    return systemLightTheme.copyWith(
+      iconTheme: getIconTheme(),
+      textTheme: getTextTheme().apply(
+        bodyColor: mainTextColoLight,
+        displayColor: mainTextColoLight,
+      ),
+    );
+  }
+}
