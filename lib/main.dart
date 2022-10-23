b77fb8f28d3c16a4a379911caac209f145c76852
uemirhanselim <uemirhanselim@gmail.com>
2022-10-23 13:49:14 +0300

    firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_master_class/bindings/initial_bindings.dart';
 import 'package:firebase_master_class/controllers/theme_controller.dart';
+import 'package:firebase_master_class/firebase_options.dart';
 import 'package:firebase_master_class/routes/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   InitialBindings().dependencies();
   runApp(const MyApp());
 }
