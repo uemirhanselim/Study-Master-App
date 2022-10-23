3a0789ac194e97ca7b3caaaf01f65daab4be0994
uemirhanselim <uemirhanselim@gmail.com>
2022-10-23 13:50:58 +0300

    question paper contoller - data fetched from ft

--- lib/controllers/question_paper/question_paper_controller.dart
+++ lib/controllers/question_paper/question_paper_controller.dart
@@ -1,8 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_master_class/firebase_ref/references.dart';
+import 'package:firebase_master_class/models/question_paper_model.dart';
 import 'package:firebase_master_class/services/firebase_storage_service.dart';
 import 'package:get/get.dart';
 
 class QuestionPaperController extends GetxController {
   final allPaperImages = <String>[].obs;
+  final allPapers = <QuestionPaperModel>[].obs;
 
   @override
   void onReady() {
@@ -11,20 +15,24 @@ class QuestionPaperController extends GetxController {
   }
 
   Future<void> getAllPapers() async {
-    List<String> imgName = [
-      "biology",
-      "chemistry",
-      "math",
-      "physics",
-    ];
-
     try {
-      for (var img in imgName) {
-        final imgUrl = await Get.find<FirebaseStorageService>().getImage(img);
-        allPaperImages.add(imgUrl!);
+      print("HELLLLOOOOOOO          1");
+      QuerySnapshot<Map<String, dynamic>> data = await questionPaperRF.get();
+      print("HELLLLOOOOOO            2" );
+      final paperList = data.docs
+          .map((paper) => QuestionPaperModel.fromSnapshot(paper))
+          .toList();
+      print("HELLLLOOOOOOO      3");
+      allPapers.assignAll(paperList);
+
+      for (var paper in paperList) {
+        final imgUrl =
+            await Get.find<FirebaseStorageService>().getImage(paper.title);
+        paper.imageUrl = imgUrl;
       }
+      allPapers.assignAll(paperList);
     } catch (e) {
-      print(e);
+      print("error occured in getAllPapers -> $e");
     }
   }
 }
