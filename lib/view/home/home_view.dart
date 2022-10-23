465bb135db32a2046b65cc466080649edcedb87d
uemirhanselim <uemirhanselim@gmail.com>
2022-10-23 13:49:59 +0300

    cni applied to homeView

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firebase_master_class/controllers/question_paper/question_paper_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -9,22 +10,29 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     QuestionPaperController questionPaperController = Get.find();
     return Scaffold(
-      body: Obx(() => ListView.separated(
-          itemBuilder: (context, index) {
-            return ClipRRect(
-              child: SizedBox(
-                height: 200,
-                width: 200,
-                child: FadeInImage(
-                    placeholder:
-                        const AssetImage("assets/images/app_splash_logo.png"),
-                    image: NetworkImage(
-                        questionPaperController.allPaperImages[index])),
-              ),
-            );
-          },
-          separatorBuilder: (context, index) => const SizedBox(height: 20),
-          itemCount: questionPaperController.allPaperImages.length),),
+      body: Obx(
+        () => ListView.separated(
+            itemBuilder: (context, index) {
+              return ClipRRect(
+                child: SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: CachedNetworkImage(
+                    placeholder: (context, url) => Container(
+                      alignment: Alignment.center,
+                      child: const CircularProgressIndicator(),
+                    ),
+                    imageUrl:
+                        questionPaperController.allPapers[index].imageUrl!,
+                    errorWidget: (context, url, error) =>
+                        Image.asset("assets/images/app_splash_logo.png"),
+                  ),
+                ),
+              );
+            },
+            separatorBuilder: (context, index) => const SizedBox(height: 20),
+            itemCount: questionPaperController.allPapers.length),
+      ),
     );
   }
 }
